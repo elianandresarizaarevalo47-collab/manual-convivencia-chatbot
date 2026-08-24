88ae14ebeb00dd419d6c4f002032c0be4a614227
elianandresarizaarevalo47-collab <elianandresarizaarevalo47@gmail.com>
2026-08-24 14:15:17 -0500

    re1

--- js/respuestas.js
+++ js/respuestas.js
@@ -1,6 +1,11 @@
 // ===================================
 // BASE DE CONOCIMIENTO DEL CHATBOT
-// Manual de Convivencia v0.8
+// Manual de Convivencia v1.0
+// ===================================
+
+
+// ===================================
+// RESPUESTAS GENERALES
 // ===================================
 
 const conocimiento = [
@@ -135,3 +140,56 @@ const conocimiento = [
     }
 
 ];
+
+
+// ===================================
+// INFORMACIÓN DEL MANUAL
+// ===================================
+
+const manual = [
+
+    {
+        titulo: "Modo Ñaura",
+        palabras: [
+            "nyu"
+        ],
+        contenido: `🦖 ¡Activaste el modo Ñaura! perez maco`
+    },
+
+
+    {
+        titulo: "Modo Ñanda",
+        palabras: [
+            "nyo"
+        ],
+        contenido: `🦖 ¡Activaste el modo Ñanda! pertuz maco`
+    }
+
+];
+
+
+// ===================================
+// FUNCIÓN PARA BUSCAR EN EL MANUAL
+// ===================================
+
+function buscarEnManual(textoUsuario) {
+
+    const pregunta = textoUsuario.toLowerCase();
+
+    for (const dato of manual) {
+
+        for (const palabra of dato.palabras) {
+
+            if (pregunta.includes(palabra.toLowerCase())) {
+
+                return dato;
+
+            }
+
+        }
+
+    }
+
+    return null;
+
+}
